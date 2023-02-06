154402eb6bac56a8d40feba87668014e7f1037c3
wangjun.byte <wangjun.byte@bytedance.com>
2023-02-06 16:55:23 +0800

    chore: init

---
+++ packages/components/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { funcA } from 'utils';
+
+export const ComponentA = () => {
+  return <>this component a，run funcA {funcA('ComponentA')}</>;
+};
---
+++ packages/utils/index.tsx
@@ -0,0 +1,3 @@
+export const funcA = (words: string) => {
+  return 'this is funcA' + words;
+};
